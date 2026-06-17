4f02796d5a943b8985ecf3b176def0fef673e230
RedaQal <redaqal@gmail.com>
2026-06-17 19:57:22 +0100

    feat: complete user API (list, delete, pagination todo)

--- src/api.py
+++ src/api.py
@@ -4,3 +4,6 @@ def ajouter_utilisateur(nom, email):
     utilisateurs.append({"nom": nom, "email": email})
     return utilisateurs
 # Mise à jour API
+def lister_utilisateurs(): return utilisateurs
+def supprimer_utilisateur(email): pass
+# TODO: add pagination
